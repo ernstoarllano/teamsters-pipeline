caa2066e68a863e0f83b654049ac1ad3c956e08d
Ernesto Arellano <ernestoarellano@icloud.com>
2019-09-22 21:05:51 -0700

    update default pagination and update members view

---
+++ resources/views/vendor/pagination/default.blade.php
@@ -0,0 +1,48 @@
+@if ($paginator->hasPages())
+    <nav class="mt-8">
+        <ul class="flex flex-row items-center justify-evenly">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="mr-3 opacity-25 disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span aria-hidden="true">&lsaquo;</span>
+                </li>
+            @else
+                <li class="mr-3">
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                </li>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li class="opacity-25 disabled" aria-disabled="true"><span>{{ $element }}</span></li>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="mx-3 active" aria-current="page">
+                                <span class="font-semibold">{{ $page }}</span>
+                            </li>
+                        @else
+                            <li class="mx-3"><a href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="ml-3">
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                </li>
+            @else
+                <li class="ml-3 opacity-25 disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span aria-hidden="true">&rsaquo;</span>
+                </li>
+            @endif
+        </ul>
+    </nav>
+@endif
